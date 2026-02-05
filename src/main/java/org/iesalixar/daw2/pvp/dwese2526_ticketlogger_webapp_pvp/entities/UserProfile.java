0f8c7cf818984e99d23516bf3a3009b62bd2ef49
Pedro Vázquez <vazquezpalominopedro@gmail.com>
2026-02-05 12:14:47 +0100

    Update UserProfile.java

--- src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/entities/UserProfile.java
+++ src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/entities/UserProfile.java
@@ -1,13 +1,43 @@
 package org.iesalixar.daw2.pvp.dwese2526_ticketlogger_webapp_pvp.entities;
 
 public class UserProfile {
-    private User user;
 
-    public void setUser(User user) {
+    private User user;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String bio;
+    private String profileImagePath;
+    private String locale;
+
+    // Constructor vacío
+    public UserProfile() {}
+
+    // Constructor con User
+    public UserProfile(User user) {
         this.user = user;
     }
 
-    public User getUser() {
-        return this.user;
-    }
+    // Getters y setters
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
+
+    public String getProfileImagePath() { return profileImagePath; }
+    public void setProfileImagePath(String profileImagePath) { this.profileImagePath = profileImagePath; }
+
+    public String getLocale() { return locale; }
+    public void setLocale(String locale) { this.locale = locale; }
+
 }
