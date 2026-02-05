b294982476be001eb5c611cae97e22bbbb91085d
Pedro Vázquez <vazquezpalominopedro@gmail.com>
2026-02-05 12:12:25 +0100

    Update UserProfile.java

--- src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/entities/UserProfile.java
+++ src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/entities/UserProfile.java
@@ -1,4 +1,13 @@
 package org.iesalixar.daw2.pvp.dwese2526_ticketlogger_webapp_pvp.entities;
 
 public class UserProfile {
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
 }
