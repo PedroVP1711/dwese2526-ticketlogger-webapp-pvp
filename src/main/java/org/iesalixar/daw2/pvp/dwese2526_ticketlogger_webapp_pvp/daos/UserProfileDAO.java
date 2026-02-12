3bd7b02f09a5f1e5aa1837b8e9e82b39085cab6a
Pedro Vázquez <vazquezpalominopedro@gmail.com>
2026-02-12 13:15:56 +0100

    Update UserProfileDAO.java

--- src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/daos/UserProfileDAO.java
+++ src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/daos/UserProfileDAO.java
@@ -2,11 +2,16 @@ package org.iesalixar.daw2.pvp.dwese2526_ticketlogger_webapp_pvp.daos;
 
 import org.iesalixar.daw2.pvp.dwese2526_ticketlogger_webapp_pvp.entities.UserProfile;
 
+/**
+ * DAO para la entidad {@link UserProfile}.
+ *
+ * Pensando para la funcionalidad de gestión de perfil de usuario ("Mi perfil")
+ * donde normalmente se trabaja con un único perfil asociado a un usuario
+ */
+
 public interface UserProfileDAO {
 
-    UserProfile findByUserId(Long userId);
-    UserProfile findByProfileId(Long id);
-    void save(UserProfile profile);
-    UserProfile update(UserProfile profile);
-    void delete(Long id);
+    UserProfile getUserProfileByUserId(Long userId);
+    void saveOrUpdateUserProfile(UserProfile userProfile);
+    boolean existsUserProfileByUserId(Long userId);
 }
