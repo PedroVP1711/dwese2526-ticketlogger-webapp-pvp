b98c018af3cb73a7f395a1111fc0dbacc6a27f96
Pedro Vázquez <vazquezpalominopedro@gmail.com>
2026-02-12 13:11:00 +0100

    Create UserProfile.java

---
+++ src/main/java/org/iesalixar/daw2/pvp/dwese2526_ticketlogger_webapp_pvp/entities/UserProfile.java
@@ -0,0 +1,47 @@
+package org.iesalixar.daw2.pvp.dwese2526_ticketlogger_webapp_pvp.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user_profiles")
+public class UserProfile {
+
+    @Id
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "Id", nullable = false)
+    private Long Id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "locale")
+    private String locale;
+
+    @Transient
+    private MultipartFile profileImageFile;
+
+    public String getProfileImage() {
+        return profileImage; // Devuelve la ruta de la imagen de perfil
+    }
+}
